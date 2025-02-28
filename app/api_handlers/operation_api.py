84c25d74a80b1d92ef8bb9aed9aaae1ccf51210e
osha890 <oshavshav@gmail.com>
2025-02-28 21:18:04 +0300

    add operations_api

---
+++ app/api_handlers/operation_api.py
@@ -0,0 +1,44 @@
+from finance_tg_bot.app.utils import handle_api_errors, get_auth_header
+from finance_tg_bot.config import API_BASE_URL
+from finance_tg_bot.session import get_session
+
+operations_url = f"{API_BASE_URL}/operations/"
+
+
+@handle_api_errors()
+async def list_operations_api(token_key, params):
+    session = get_session()
+    return await session.get(url=operations_url,
+                             params=params,
+                             headers=get_auth_header(token_key))
+
+
+@handle_api_errors()
+async def create_operation_api(token_key, json_data):
+    session = get_session()
+    return await session.post(url=operations_url,
+                              headers=get_auth_header(token_key),
+                              json=json_data)
+
+
+@handle_api_errors()
+async def delete_operation_api(token_key, operation_id):
+    session = get_session()
+    return await session.delete(url=f"{operations_url}{operation_id}/",
+                                headers=get_auth_header(token_key))
+
+
+@handle_api_errors()
+async def update_operation_api(token_key, operation_id, json_data):
+    session = get_session()
+    return await session.patch(url=f"{operations_url}{operation_id}/",
+                               headers=get_auth_header(token_key),
+                               json=json_data)
+
+
+@handle_api_errors()
+async def get_recent_operations_api(token_key, params):
+    session = get_session()
+    return await session.get(url=f"{operations_url}recent/",
+                             params=params,
+                             headers=get_auth_header(token_key))
