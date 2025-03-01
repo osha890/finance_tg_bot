399418373841f35aacacce9b9a6fb64e59cfc311
osha890 <oshavshav@gmail.com>
2025-03-01 15:59:41 +0300

    add make_answer_category
    common for category

--- app/commands/finance_commands/category_commands.py
+++ app/commands/finance_commands/category_commands.py
@@ -21,66 +21,43 @@ from ...states import (GetCategoriesState,
 router = Router()
 
 
-# ======== ANSWER MAKERS ===============================================================================
+# ======== ANSWER MAKER ===============================================================================
 
-async def make_answer_list_categories(response):
+async def make_answer_category(response, messages_item):
     if type(response) == str:
         return response
 
-    response_data = await response.json()
-    if response.status == 200:
-        categories = response_data
-        if categories:
+    rs = response.status
+    if rs == 200:
+        response_data = await response.json()
+        if response_data and type(response_data) == list:
+            categories = response_data
             answer_text = "\n".join(
                 [f"ID: {category['id']} - {category['name']}: {get_type(category['type'])}" for category in categories])
+        elif type(response_data) == dict:
+            category = response_data
+            answer_text = markdown.text(
+                messages_item["updated"],
+                f"ID: {category['id']} - {category['name']}: {get_type(category['type'])}",
+                sep="\n"
+            )
         else:
-            answer_text = messages.NO_CATEGORIES
+            answer_text = messages_item["no_items"]
+    elif rs == 201:
+        category = await response.json()
+        answer_text = markdown.text(
+            messages_item["added"],
+            f"ID: {category['id']} - {category['name']}: {get_type(category['type'])}",
+            sep="\n"
+        )
+    elif rs == 204:
+        answer_text = messages_item["deleted"]
+    elif rs == 403:
+        answer_text = messages_item["cant_change"]
+    elif rs == 404:
+        answer_text = messages_item["not_found"]
     else:
-        answer_text = make_error_answer(response_data)
-    return answer_text
-
-
-async def make_answer_create_category(response):
-    if type(response) == str:
-        return response
-
-    if response.status == 201:
-        answer_text = messages.CATEGORY_ADDED
-    else:
-        response_data = await response.json()
-        answer_text = make_error_answer(response_data)
-    return answer_text
-
-
-async def make_answer_delete_category(response):
-    if type(response) == str:
-        return response
-
-    if response.status == 204:
-        answer_text = messages.CATEGORY_DELETED
-    elif response.status == 403:
-        answer_text = messages.CATEGORY_CANT_CHANGE
-    elif response.status == 404:
-        answer_text = messages.CATEGORY_NOT_FOUND
-    else:
-        response_data = await response.json()
-        answer_text = make_error_answer(response_data)
-    return answer_text
-
-
-async def make_answer_update_category(response):
-    if type(response) == str:
-        return response
-
-    if response.status == 200:
-        answer_text = messages.CATEGORY_UPDATED
-    elif response.status == 403:
-        answer_text = messages.CATEGORY_CANT_CHANGE
-    elif response.status == 404:
-        answer_text = messages.CATEGORY_NOT_FOUND
-    else:
-        response_data = await response.json()
-        answer_text = make_error_answer(response_data)
+        answer_text = make_error_answer(await response.json())
     return answer_text
 
 
@@ -116,7 +93,7 @@ async def choose_category_type(message: Message, state: FSMContext):
         pass
 
     response = await list_categories_api(token_key, params)
-    answer_text = await make_answer_list_categories(response)
+    answer_text = await make_answer_category(response, messages.MESSAGES_CATEGORY)
 
     await message.answer(answer_text)
 
@@ -163,7 +140,7 @@ async def create_category_type(message: Message, state: FSMContext):
             "type": category_type
         }
         response = await create_category_api(token_key, json_data)
-        answer_text = await make_answer_create_category(response)
+        answer_text = await make_answer_category(response, messages.MESSAGES_CATEGORY)
         await message.answer(answer_text)
     else:
         await message.answer(messages.CATEGORY_WRONG_TYPE)
@@ -190,7 +167,7 @@ async def delete_category_confirm(message: Message, state: FSMContext):
     category_id = message.text.strip()
 
     response = await delete_category_api(token_key, category_id)
-    answer_text = await make_answer_delete_category(response)
+    answer_text = await make_answer_category(response, messages.MESSAGES_CATEGORY)
     await message.answer(answer_text)
 
     await state.clear()
@@ -223,7 +200,7 @@ async def update_category_type(message: Message, state: FSMContext):
 
     json_data = {"name": category_name}
     response = await update_category_api(token_key, category_id, json_data)
-    answer_text = await make_answer_update_category(response)
+    answer_text = await make_answer_category(response, messages.MESSAGES_CATEGORY)
     await message.answer(answer_text)
 
     await state.clear()
