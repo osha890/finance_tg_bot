5d1076f16e9dbab54b7bcf17387aa42f49417889
osha890 <oshavshav@gmail.com>
2025-03-05 18:16:07 +0300

    update requirements

--- database/crud.py
+++ database/crud.py
@@ -6,26 +6,10 @@ from sqlalchemy.future import select
 from .models import UserToken
 
 
-# def get_token(db: Session, user_id: int):
-#     return db.query(UserToken).filter(UserToken.user_id == user_id).first()
-
 async def get_token(db: AsyncSession, user_id: int):
     result = await db.execute(select(UserToken).filter(UserToken.user_id == user_id))
     return result.scalars().first()
 
-
-# def save_token(db: Session, user_id: int, key: str):
-#     user_token = get_token(db, user_id)
-#     if user_token:
-#         user_token.key = key
-#     else:
-#         user_token = UserToken(user_id=user_id, key=key)
-#         db.add(user_token)
-#
-#     try:
-#         db.commit()
-#     except IntegrityError:
-#         db.rollback()
 
 async def save_token(db: AsyncSession, user_id: int, key: str):
     user_token = await get_token(db, user_id)
--- database/db_settings.py
+++ database/db_settings.py
@@ -7,27 +7,14 @@ from sqlalchemy.orm import sessionmaker
 from config import DATABASE_URL, DATABASE_URL_ASYNC
 from .models import Base
 
-# engine = create_engine(DATABASE_URL, echo=True)
-# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 engine = create_async_engine(DATABASE_URL_ASYNC, echo=True)
 SessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
 
-
-# def init_db():
-#     Base.metadata.create_all(bind=engine)
 
 async def init_db():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
 
-# @contextmanager
-# def get_db():
-#     db = SessionLocal()
-#     try:
-#         yield db
-#     finally:
-#         db.close()
 
 @asynccontextmanager
 async def get_db():
